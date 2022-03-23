a13816aeb6fca21343ecece540693399bacaf489
Antonio L <256megas@gmail.com>
2022-03-23 17:52:00 +0100

    Refactoring

--- src/components/search.js
+++ src/components/search.js
@@ -1,57 +1,20 @@
 import React, { Component } from "react";
+import defaultState from "../providers/defaultState.js";
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gallery: this.defaultState,
+      gallery: defaultState,
     };
   }
-
-  defaultState = [
-    {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3qcDJfXEtNWxaeewGXYj1TJIMOFMMHwkRwg&usqp=CAU",
-      title: "Batman",
-      keywords: ["dc", "heroe", "batman"],
-    },
-    {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5yvvB-alfJcmu5ubbq61mJeZHDJ9GCl1WcQ&usqp=CAU",
-      title: "Doctor Doom",
-      keywords: ["marvel", "villano", "doom"],
-    },
-    {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj2wMU8ZGGytfzeHT7U4fPZTcdtW2ydxUxYw&usqp=CAU",
-      title: "Mistica",
-      keywords: ["marvel", "villano", "mistica"],
-    },
-    {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG4wRi43c3taiv-9ttrKq0117MnzsWvsFnQ&usqp=CAU",
-      title: "Green Arrow",
-      keywords: ["dc", "heroe", "green arrow"],
-    },
-    {
-      img: "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/08/Tim-Drake-uno-de-los-Robin-de-Batman-es-oficialmente-parte-de-la-comunidad-LGBT.jpg?resize=1280%2C720&quality=80&ssl=1",
-      title: "Robin",
-      keywords: ["dc", "heroe", "robin"],
-    },
-    {
-      img: "https://super-ficcion.com/wp-content/uploads/2021/12/DCs-Nightwing-Filmdrehbuch-ist-eine-Ur-Rache-Geschichte2webp-780x470.webp",
-      title: "NightWing",
-      keywords: ["dc", "heroe", "NightWing", "Robin"],
-    },
-    {
-      img: "https://dam.smashmexico.com.mx/wp-content/uploads/2020/09/Silk-serie-marvel-sony-pictures-plataformas-digitales-cover-770x513.jpg",
-      title: "Silk",
-      keywords: ["marvel", "heroe", "Silk"],
-    },
-  ];
 
   buscar(e) {
     var temporalGallery = this.state.gallery;
     var resultGallery = [];
     if (e.target.value === "") {
       this.setState({
-        gallery: this.defaultState,
+        gallery: defaultState,
       });
     } else {
       temporalGallery.forEach(function (element) {
---
+++ src/providers/defaultState.js
@@ -0,0 +1,39 @@
+const defaultState = [
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3qcDJfXEtNWxaeewGXYj1TJIMOFMMHwkRwg&usqp=CAU",
+    title: "Batman",
+    keywords: ["dc", "heroe", "batman"],
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5yvvB-alfJcmu5ubbq61mJeZHDJ9GCl1WcQ&usqp=CAU",
+    title: "Doctor Doom",
+    keywords: ["marvel", "villano", "doom"],
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj2wMU8ZGGytfzeHT7U4fPZTcdtW2ydxUxYw&usqp=CAU",
+    title: "Mistica",
+    keywords: ["marvel", "villano", "mistica"],
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG4wRi43c3taiv-9ttrKq0117MnzsWvsFnQ&usqp=CAU",
+    title: "Green Arrow",
+    keywords: ["dc", "heroe", "green arrow"],
+  },
+  {
+    img: "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/08/Tim-Drake-uno-de-los-Robin-de-Batman-es-oficialmente-parte-de-la-comunidad-LGBT.jpg?resize=1280%2C720&quality=80&ssl=1",
+    title: "Robin",
+    keywords: ["dc", "heroe", "robin"],
+  },
+  {
+    img: "https://super-ficcion.com/wp-content/uploads/2021/12/DCs-Nightwing-Filmdrehbuch-ist-eine-Ur-Rache-Geschichte2webp-780x470.webp",
+    title: "NightWing",
+    keywords: ["dc", "heroe", "NightWing", "Robin"],
+  },
+  {
+    img: "https://dam.smashmexico.com.mx/wp-content/uploads/2020/09/Silk-serie-marvel-sony-pictures-plataformas-digitales-cover-770x513.jpg",
+    title: "Silk",
+    keywords: ["marvel", "heroe", "Silk"],
+  },
+];
+
+export default defaultState;
